d5755c580a699a322829349ba83d0456b57a70ee
William Palmer <will@steelhive.com>
2016-06-07 12:25:38 -0400

    allow multiple comma-separated cli messages

--- bin/irc-cmd.js
+++ bin/irc-cmd.js
@@ -8,11 +8,21 @@ cmd.version('0.1.4')
     .option('-s --server [server]')
     .option('-c --channel [channel]')
     .option('-n --nickname [nick]')
-    .option('-m --message [message]')
+    .option('-m --message [message]', 'a single message or comma-separated list of messages')
     .parse(process.argv);
 
+var messages = cmd.message
+    .split(',')
+    .map(function (e) { return e.trim() });
+
 var client = new irccmd.Client(cmd.server, cmd.channel, cmd.nickname)
-client.send(cmd.message);
+
+cmd.message
+    .split(',')
+    .forEach(function (msg) {
+        client.send(msg.trim());
+    });
+
 client.on('drained', function() {
     client.disconnect();
     process.exit(0);
